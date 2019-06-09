9543ff074393d6c6a8c13aa04ea1ba558d69f631
kaiorosa1 <kaiosrosa@gmail.com>
2019-06-09 12:22:26 -0300

    add Docente attributes

--- src/sistemacredenciamento/model/Docente.java
+++ src/sistemacredenciamento/model/Docente.java
@@ -5,10 +5,59 @@
  */
 package sistemacredenciamento.model;
 
+import java.util.Date;
+
 /**
  *
  * @author Kaio Rosa
  */
 public class Docente {
+    private int codigo;
+    private String nome;
+    private Date dataIngresso;
+    private boolean isCordenador;
+
+    
+    public Docente(int codigo, String nome, Date dataIngresso, boolean isCordenador) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.dataIngresso = dataIngresso;
+        this.isCordenador = isCordenador;
+    }
+
+    
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataIngresso() {
+        return dataIngresso;
+    }
+
+    public void setDataIngresso(Date dataIngresso) {
+        this.dataIngresso = dataIngresso;
+    }
+
+    public boolean isIsCordenador() {
+        return isCordenador;
+    }
+
+    public void setIsCordenador(boolean isCordenador) {
+        this.isCordenador = isCordenador;
+    }
+    
     
 }
