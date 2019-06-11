c246da77185d209d0e815b8be11f46486c2c520b
kaiorosa1 <kaiosrosa@gmail.com>
2019-06-10 22:31:38 -0300

    start reading docentes.csv

--- src/sistemacredenciamento/controller/Main.java
+++ src/sistemacredenciamento/controller/Main.java
@@ -1,5 +1,10 @@
 package sistemacredenciamento.controller;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,8 +22,37 @@ public class Main {
      */
     public static void main(String[] args) {
        // Read Files and create the objects
+       String fileName = "C:\\Users\\user\\Documents\\NetBeansProjects\\SistemaCredenciamentoPPGI\\src\\sistemacredenciamento\\docentes.csv";
        
-       // create handle exceptions
+       BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ";";
+
+        try {
+
+            br = new BufferedReader(new FileReader(fileName));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] codigo = line.split(cvsSplitBy);
+
+                System.out.println(codigo[0]);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
        
        
     }
