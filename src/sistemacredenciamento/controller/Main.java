2e4cd35c9c33f132fcda3bfaf9ba9f29ac5b389f
kaiorosa1 <kaiosrosa@gmail.com>
2019-06-13 14:49:45 -0300

    add listaDocentes populated

--- src/sistemacredenciamento/controller/Main.java
+++ src/sistemacredenciamento/controller/Main.java
@@ -9,6 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import sistemacredenciamento.model.*;
@@ -31,15 +32,16 @@ public class Main {
     public static void main(String[] args) {
        // Read Files and create the objects
        String fileName = "C:\\Users\\user\\Documents\\NetBeansProjects\\SistemaCredenciamentoPPGI\\src\\sistemacredenciamento\\docentes.csv";
-        try {
-            List<Docente> listaDocentes = readDocentes(fileName);
+        List<Docente> listaDocentes = null;
+       try {
+            listaDocentes = readDocentes(fileName);
             
         } catch (ParseException ex) {
             System.out.println("Error ao converter data");
         }
        
        
-       
+//       printListaDocente(listaDocentes);
     }
     
     public static List<Docente> readDocentes(String fName) throws ParseException{
@@ -56,9 +58,11 @@ public class Main {
             line = br.readLine();
             while ((line = br.readLine()) != null) {
 
-                String[] codigo = line.split(cvsSplitBy);
-                if(codigo[4].equals("X")){
+                String[] codigo = line.split(cvsSplitBy,'\n');
+               
+                if(Objects.equals(codigo[4],"X")){
                     listaDocentes.add( new Docente(Long.parseLong(codigo[0]), codigo[1],new SimpleDateFormat("dd/MM/yyyy").parse(codigo[2]),new SimpleDateFormat("dd/MM/yyyy").parse(codigo[3]),true));
+                    
                 }else{
                     listaDocentes.add( new Docente(Long.parseLong(codigo[0]), codigo[1],new SimpleDateFormat("dd/MM/yyyy").parse(codigo[2]),new SimpleDateFormat("dd/MM/yyyy").parse(codigo[3]),false));
                 
@@ -70,6 +74,14 @@ public class Main {
         } 
         
         return listaDocentes;
+    }
+    
+    public static void printListaDocente(List<Docente> list){
+        
+        list.forEach((l) -> {
+            System.out.println(l.getNome());
+        });
+        
     }
 }
 
