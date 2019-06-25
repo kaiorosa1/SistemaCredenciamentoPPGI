63056c3a176a6ea8ddb2a572309d56ee066fd5d4
kaiorosa1 <kaiosrosa@gmail.com>
2019-06-25 16:23:54 -0300

    add connection to database

---
+++ src/sistemacredenciamento/connection/DBConnection.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemacredenciamento.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author user
+ */
+public class DBConnection {
+    public static Connection conectarMysql() throws SQLException, ClassNotFoundException{
+        
+        //criar a condição com o banco de dados
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/cadastro","root","root");
+                
+       //faz com que classe seja carregada pela JVM
+       Class.forName("com.mysql.jdbc.Driver");
+       return connection;
+    }
+}
---
+++ src/sistemacredenciamento/connection/TesteDBConnection.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemacredenciamento.connection;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Kaio Rosa
+ */
+public class TesteDBConnection {
+    /**
+     * @param args the command line arguments
+     */
+//     teste se esta conectando com o banco
+    public static void main(String[] args) throws ClassNotFoundException {
+        // TODO code application logic here
+        try {
+            Connection conn = DBConnection.conectarMysql();
+            JOptionPane.showMessageDialog(null, "Conectado com sucesso!!");         
+            
+        } catch (SQLException ex) {
+            
+            System.out.println(ex.getMessage());
+            JOptionPane.showMessageDialog(null, "Erro ao conectar\n"+ex);
+        }
+    }
+}
