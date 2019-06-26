e83fd3e4e5758ee9c323947245e851514e99ae96
kaiorosa1 <kaiosrosa@gmail.com>
2019-06-26 13:33:28 -0300

    add salvarDocente

--- src/sistemacredenciamento/dao/DocenteDao.java
+++ src/sistemacredenciamento/dao/DocenteDao.java
@@ -5,10 +5,45 @@
  */
 package sistemacredenciamento.dao;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import sistemacredenciamento.connection.DBConnection;
+import sistemacredenciamento.model.Docente;
+
 /**
  *
  * @author Kaio Rosa
  */
 public class DocenteDao {
-    
+
+    public void salvarDocente(Docente docente) {
+        //conectar com o banco de dados aqui
+        String sql = "INSERT INTO docente(codigo, nome, dataNascimento, dataIngresso, isCordenador) VALUES(?,?,?,?,?)";
+        Connection conn = null;
+        PreparedStatement pstm = null;
+
+        try {
+            conn = DBConnection.conectarMysql();
+            pstm = conn.prepareStatement(sql);
+            pstm.setLong(1, docente.getCodigo());
+            pstm.setString(2,docente.getNome());
+            pstm.setDate(3, (Date) docente.getDataNacimeto());
+            pstm.setDate(4, (Date) docente.getDataIngresso());
+            pstm.setBoolean(5,docente.isIsCordenador());
+            pstm.execute();
+
+        } catch (Exception e) {
+
+            e.getMessage();
+        } finally {
+            try {
+                if (pstm != null) {
+                    pstm.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+
+    }
 }
