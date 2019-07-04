4c4b77e13d8e3671427768c9f07cd3e88acfadd5
kaiorosa1 <kaiosrosa@gmail.com>
2019-07-03 22:39:35 -0300

    add listarVeiculos

--- src/sistemacredenciamento/dao/VeiculoDao.java
+++ src/sistemacredenciamento/dao/VeiculoDao.java
@@ -8,7 +8,11 @@ package sistemacredenciamento.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import sistemacredenciamento.connection.DBConnection;
+import sistemacredenciamento.model.Docente;
 import sistemacredenciamento.model.Veiculo;
 
 /**
@@ -43,6 +47,49 @@ public class VeiculoDao {
             } catch (Exception e) {
             }
         }
+
+    }
+      public List<Veiculo> listarVeiculo() {
+        //INCOMPLETE
+        List<Veiculo> listaVeiculos = new ArrayList<>();
+        String sql = "SELECT * FROM veiculo";
+        Connection conn = null;
+        PreparedStatement pstm = null;
+
+        //Classe que vai recuperar os dados do banco de dados
+        ResultSet rset = null;
+
+        Docente docente = new Docente();
+
+        try {
+            conn = DBConnection.conectarMysql();
+
+            pstm = conn.prepareStatement(sql);
+            rset = pstm.executeQuery();
+
+            while (rset.next()) {
+
+               
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (rset != null) {
+                    rset.close();
+                }
+                if (pstm != null) {
+                    pstm.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return listaVeiculos;
 
     }
 }
